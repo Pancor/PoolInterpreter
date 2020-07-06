79b10d8e2ce0bee53c48a74520ffe0148863f87b
Pablo <pancordev@gmail.com>
2020-07-06 18:36:05 +0200

    Added button to move to GameplayActivity.

--- app/src/main/java/pl/pancordev/poolinterpreter/MainActivity.kt
+++ app/src/main/java/pl/pancordev/poolinterpreter/MainActivity.kt
@@ -1,71 +1,19 @@
 package pl.pancordev.poolinterpreter
 
-import android.Manifest
-import android.content.pm.PackageManager
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.act_main.*
-import org.opencv.android.BaseLoaderCallback
-import org.opencv.android.CameraBridgeViewBase
-import org.opencv.android.LoaderCallbackInterface
-import org.opencv.core.Mat
-import timber.log.Timber
+import pl.pancordev.poolinterpreter.gameplay.GameplayActivity
 
-class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListener2 {
-
-    private lateinit var managerCallback: BaseLoaderCallback
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_main)
 
-        val self = this
-        managerCallback = object : BaseLoaderCallback(this) {
-            override fun onManagerConnected(status: Int) {
-                Timber.d("OpenCV manager connection status: $status")
-                when (status) {
-                    LoaderCallbackInterface.SUCCESS ->  ActivityCompat.requestPermissions(self, arrayOf(Manifest.permission.CAMERA), 1)
-                    else ->  super.onManagerConnected(status)
-                }
-            }
+        gameplayBtn.setOnClickListener {
+            startActivity(Intent(this, GameplayActivity::class.java))
         }
-        cameraView.setCvCameraViewListener(this)
-    }
-
-    override fun onCameraViewStarted(width: Int, height: Int) {
-        Timber.d("CameraView started")
-    }
-
-    override fun onCameraViewStopped() {
-        Timber.d("CameraView stopped")
-    }
-
-    override fun onCameraFrame(inputFrame: CameraBridgeViewBase.CvCameraViewFrame): Mat {
-        return inputFrame.rgba()
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when (requestCode) {
-            1 ->
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Timber.d("Camera permissions granted")
-                    cameraView.setCameraPermissionGranted()
-                    cameraView.enableView()
-                } else {
-                    Timber.e("Permissions not granted for camera")
-                }
-            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        cameraView.disableView()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        cameraView.disableView()
     }
 }
