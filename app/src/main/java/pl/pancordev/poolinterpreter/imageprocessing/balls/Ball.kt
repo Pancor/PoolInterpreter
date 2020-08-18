d6d13415ec84729bb2227bba70819e631ad9ea1e
Pancor <pancordev@gmail.com>
2020-08-18 20:34:46 +0200

    Added properties to hold ball type.

--- app/src/main/java/pl/pancordev/poolinterpreter/imageprocessing/balls/Ball.kt
+++ app/src/main/java/pl/pancordev/poolinterpreter/imageprocessing/balls/Ball.kt
@@ -2,4 +2,12 @@ package pl.pancordev.poolinterpreter.imageprocessing.balls
 
 import org.opencv.core.Point
 
-data class Ball(val center: Point, val radius: Int)
+data class Ball(val id: Int, val center: Point, val radius: Int) {
+    var color: BallType = BallType.UNKNOWN
+    var isStriped = false
+    var whiteArea = 0.0
+
+    var ballColor1 = 0.0
+    var ballColor2 = 0.0
+    var ballColor3 = 0.0
+}
