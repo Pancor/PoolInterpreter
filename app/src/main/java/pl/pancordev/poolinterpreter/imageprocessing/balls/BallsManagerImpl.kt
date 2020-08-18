c9ef7cd5543049b94df77de2e353f26b57b519be
Pancor <pancordev@gmail.com>
2020-08-18 20:36:02 +0200

    Implemented ball recognition.

--- app/src/main/java/pl/pancordev/poolinterpreter/imageprocessing/balls/BallsManagerImpl.kt
+++ app/src/main/java/pl/pancordev/poolinterpreter/imageprocessing/balls/BallsManagerImpl.kt
@@ -8,6 +8,7 @@ class BallsManagerImpl : BallsContract.BallsManager {
 
     private var ratio = 0.0
     private var hacked = Mat()
+    var balls = mutableListOf<Ball>()
 
     override fun getBalls(mat: Mat): List<Ball> {
         val resizedMat = Mat()
@@ -24,23 +25,22 @@ class BallsManagerImpl : BallsContract.BallsManager {
         val circles = Mat()
         Imgproc.HoughCircles(blurred, circles, Imgproc.HOUGH_GRADIENT, 1.0,
             20.0, 50.0, 17.0,
-            5, 13)
+            8, 13)
         Timber.d("Found ${circles.cols()} balls")
 
-        val balls = mutableListOf<Ball>()
+        balls.clear()
         //val size = if (circles.cols() > 16) { 16 } else { circles.cols() }
         val size = if (circles.cols() > 9) { 9 } else { circles.cols() }
-        for (x in 0 until size) { //16 because there is only 16 balls
+        for (x in 0 until size) {
             val c = circles.get(0, x)
             val center = Point(Math.round(c[0]*ratio).toDouble(), Math.round(c[1]*ratio).toDouble())
             val radius = Math.round(c[2]*ratio).toInt()
-            balls.add(Ball(center, radius))
+            balls.add(Ball(x, center, radius))
         }
 
         for (ball in balls) {
             val ballSize = ball.radius * 2
             val contours = Rect((ball.center.x - ball.radius).toInt(), (ball.center.y - ball.radius).toInt(), ballSize, ballSize)
-            Timber.d("Ball: ${ball.radius} (${ball.center.x}, ${ball.center.y}), mat: (${mat.size().width}, ${mat.size().height})")
             val roi = Mat(mat, contours)
 
             val roiHsvMat = Mat()
@@ -48,69 +48,91 @@ class BallsManagerImpl : BallsContract.BallsManager {
 
             val mask = Mat.zeros(roi.size(), CvType.CV_8UC1)
             Imgproc.circle(mask, Point(ball.radius.toDouble(), ball.radius.toDouble()), ball.radius, Scalar(255.0, 255.0, 255.0), -1)
-            roi.copyTo(roi, mask)
+
+            val roiGray = Mat()
+            Imgproc.cvtColor(roi, roiGray, Imgproc.COLOR_RGB2GRAY)
+            val roiRemoveWhite = Mat()
+            Imgproc.threshold(roiGray, roiRemoveWhite, 200.0, 255.0, Imgproc.THRESH_BINARY_INV)
+            val finalMask = Mat()
+            Core.bitwise_and(mask, roiRemoveWhite, finalMask)
+
+            val whiteCnts: List<MatOfPoint> = mutableListOf()
+            Core.bitwise_not(roiRemoveWhite, roiRemoveWhite)
+            Imgproc.findContours(roiRemoveWhite, whiteCnts, Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE)
+            for (cnt in whiteCnts) {
+                ball.whiteArea += Imgproc.contourArea(cnt)
+            }
 
             val hist1 = Mat()
-            Imgproc.calcHist(listOf(roiHsvMat), MatOfInt(0), mask, hist1, MatOfInt(180), MatOfFloat(0f, 180f))
+            Imgproc.calcHist(listOf(roiHsvMat), MatOfInt(0), finalMask, hist1, MatOfInt(180), MatOfFloat(0f, 180f))
             val minMaxLocResult = Core.minMaxLoc(hist1)
-            val ballColor1 = minMaxLocResult.maxLoc.y
+            ball.ballColor1 = minMaxLocResult.maxLoc.y
 
             val hist2 = Mat()
-            Imgproc.calcHist(listOf(roiHsvMat), MatOfInt(1), mask, hist2, MatOfInt(255), MatOfFloat(0f, 255f))
+            Imgproc.calcHist(listOf(roiHsvMat), MatOfInt(1), finalMask, hist2, MatOfInt(255), MatOfFloat(0f, 255f))
             val minMaxLocResult2 = Core.minMaxLoc(hist2)
-            val ballColor2 = minMaxLocResult2.maxLoc.y
+            ball.ballColor2 = minMaxLocResult2.maxLoc.y
 
             val hist3 = Mat()
-            Imgproc.calcHist(listOf(roiHsvMat), MatOfInt(2), mask, hist3, MatOfInt(255), MatOfFloat(0f, 255f))
+            Imgproc.calcHist(listOf(roiHsvMat), MatOfInt(2), finalMask, hist3, MatOfInt(255), MatOfFloat(0f, 255f))
             val minMaxLocResult3 = Core.minMaxLoc(hist3)
-            val ballColor3 = minMaxLocResult3.maxLoc.y
+            ball.ballColor3 = minMaxLocResult3.maxLoc.y
 
+            if (ball.ballColor1 > 20 && ball.ballColor1 < 35) {
+                ball.color = BallType.YELLOW
+            }
+            if (ball.ballColor1 > -1 && ball.ballColor1 < 9 || ball.ballColor1 > 177) {
+                ball.color = BallType.RED
+            }
+            if (ball.ballColor1 > 6 && ball.ballColor1 < 20) {
+                ball.color = BallType.ORANGE
+            }
+            if (ball.ballColor1 > 165 && ball.ballColor1 < 178) {
+                ball.color = BallType.BROWN
+            }
+            if (ball.ballColor1 > 70 && ball.ballColor1 < 95) {
+                ball.color = BallType.GREEN
+            }
+            if (ball.ballColor1 > 100 && ball.ballColor1 < 115) {
+                ball.color = BallType.BLUE
+            }
+            if (ball.ballColor1 > 115 && ball.ballColor1 < 140) {
+                ball.color = BallType.PURPLE
+            }
+            if (ball.ballColor1 > 50 && ball.ballColor1 < 95 && ball.ballColor3 < 40) {
+                ball.color = BallType.BLACK
+            }
+            if (ball.ballColor2 < 50) {
+                ball.color = BallType.WHITE
+            }
+            isStriped(ball)
+        }
 
-            Timber.d("Ball color: ($ballColor1, $ballColor2, $ballColor3)")
-
-
-            var properType = BallType.UNKNOWN
-            if (ballColor1 > 25 && ballColor1 < 35) {
-                properType = BallType.YELLOW
-            }
-            if (ballColor1 > -1 && ballColor1 < 9) {
-                properType = BallType.RED
-            }
-            if (ballColor1 > 10 && ballColor1 < 24) {
-                properType = BallType.ORANGE
-            }
-            if (ballColor1 > 165 && ballColor1 < 180) {
-                properType = BallType.BROWN
-            }
-            if (ballColor1 > 70 && ballColor1 < 95) {
-                properType = BallType.GREEN
-            }
-            if (ballColor1 > 100 && ballColor1 < 115) {
-                properType = BallType.BLUE
-            }
-            if (ballColor1 > 115 && ballColor1 < 140) {
-                properType = BallType.PURPLE
-            }
-            if (ballColor3 < 40) {
-                properType = BallType.BLACK
-            }
-            if (ballColor2 < 50) {
-                properType = BallType.WHITE
-            }
-
+        for (ball in balls) {
             Imgproc.circle(mat, ball.center, ball.radius, Scalar(255.0, 0.0, 255.0),
                 3, 8, 0)
 
             val textPoint = Point(ball.center.x + 20, ball.center.y + 10)
-            if (properType != BallType.UNKNOWN) {
-                Imgproc.putText(mat, "($ballColor1, $ballColor2, $ballColor3) ${properType.name}", textPoint, Imgproc.FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255.0, 0.0, 255.0), 2)
+            if (ball.color != BallType.UNKNOWN) {
+                Imgproc.putText(mat, "(${ball.ballColor1}, ${ball.ballColor2}, ${ball.ballColor3}) ${ball.color.name} ${ball.whiteArea}, ${ball.isStriped}", textPoint, Imgproc.FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255.0, 0.0, 255.0), 2)
             } else {
-                Imgproc.putText(mat, "($ballColor1, $ballColor2, $ballColor3)", textPoint, Imgproc.FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255.0, 0.0, 255.0), 2)
+                Imgproc.putText(mat, "(${ball.ballColor1}, ${ball.ballColor2}, ${ball.ballColor3})", textPoint, Imgproc.FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255.0, 0.0, 255.0), 2)
             }
         }
-
         hacked = mat
         return balls
+    }
+
+    private fun isStriped(ball: Ball) {
+        for (it in balls) {
+            if (it.color == ball.color && it.id != ball.id ) {
+                if (ball.whiteArea > it.whiteArea) {
+                    ball.isStriped = true
+                } else if (ball.whiteArea < it.whiteArea) {
+                    it.isStriped = true
+                }
+            }
+        }
     }
 
     override fun recogniseBall(ball: Mat) {
